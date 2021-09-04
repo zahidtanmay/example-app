70e1268119c5fef76e722bd83fb3faeb93ecc37d
jahid <zahidtanmay009@gmail.com>
2021-09-04 16:14:29 +0600

    product table migration

---
+++ database/migrations/2021_09_04_092844_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->integer('user_id', false);
+            $table->string('photo_path', 255);
+            $table->timestamp('manufactured_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
